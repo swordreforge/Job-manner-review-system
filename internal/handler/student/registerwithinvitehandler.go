4c4d56e5cf39f1eb5b98e2775562ab20251ba39a
swordreforge <zhujian_20060818@qq.com>
2026-04-16 11:03:11 +0800

    add RegisterWithInviteHandler

---
+++ internal/handler/student/registerwithinvitehandler.go
@@ -0,0 +1,28 @@
+package student
+
+import (
+	"net/http"
+
+	"career-api/internal/logic/student"
+	"career-api/internal/svc"
+	"career-api/internal/types"
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func RegisterWithInviteHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.RegisterWithInviteReq
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := student.NewRegisterWithInviteLogic(r.Context(), svcCtx)
+		resp, err := l.RegisterWithInvite(&req)
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
