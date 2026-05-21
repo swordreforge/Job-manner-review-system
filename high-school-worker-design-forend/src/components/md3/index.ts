b21a07ce6948e118cd77ac2800a1519c519388d4
swordreforge <zhujian_20060818@qq.com>
2026-05-21 17:59:09 +0800

    feat: add barrel export for MD3 foundation components

---
+++ high-school-worker-design-forend/src/components/md3/index.ts
@@ -0,0 +1,8 @@
+export { default as PageHeader } from '../PageHeader';
+export { default as SurfaceCard } from '../SurfaceCard';
+export { default as StatCard } from '../StatCard';
+export { default as SkeletonLoader } from '../SkeletonLoader';
+export { default as ApiErrorState } from '../ApiErrorState';
+export { getEChartsM3Theme, getM3ChartColor, M3_CHART_COLORS } from '../EChartsM3Theme';
+export { useBreadcrumbs } from '../../hooks/useBreadcrumbs';
+export type { BreadcrumbItem } from '../../hooks/useBreadcrumbs';
